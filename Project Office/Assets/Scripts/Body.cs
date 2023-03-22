7cfccfd6b9cb3a8ccb6c9e9e1f6537dec1b36a3f
ansokols <ansokols@gmail.com>
2023-03-22 18:35:57 +0200

    Initial project

---
+++ Project Office/Assets/Scripts/Body.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour
+{
+    private Vector2 moveInput;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (moveInput.x == 0 && moveInput.y == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
+
+        if (Input.GetKey("1"))
+        {
+            anim.SetLayerWeight(anim.GetLayerIndex("Flashlight"), 1);
+            anim.SetLayerWeight(anim.GetLayerIndex("Knife"), 0);
+            anim.SetLayerWeight(anim.GetLayerIndex("Pistol"), 0);
+        }
+        else if (Input.GetKey("2"))
+        {
+            anim.SetLayerWeight(anim.GetLayerIndex("Flashlight"), 0);
+            anim.SetLayerWeight(anim.GetLayerIndex("Knife"), 1);
+            anim.SetLayerWeight(anim.GetLayerIndex("Pistol"), 0);
+        }
+        else if (Input.GetKey("3"))
+        {
+            anim.SetLayerWeight(anim.GetLayerIndex("Flashlight"), 0);
+            anim.SetLayerWeight(anim.GetLayerIndex("Knife"), 0);
+            anim.SetLayerWeight(anim.GetLayerIndex("Pistol"), 1);
+        }
+
+        anim.SetBool("isShooting", Input.GetButton("Fire1"));
+        anim.SetBool("isReloading", Input.GetKey(KeyCode.R));
+    }
+}
---
+++ Project Office/Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float runningSpeed;
+
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+    private Vector2 mousePos;
+    private Vector2 lookDir;
+    
+    private Rigidbody2D rb;
+    private Animator anim;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        float deltaAngle = Vector2.SignedAngle(moveInput, lookDir);
+        if (deltaAngle >= 60 && deltaAngle <= 120)
+        {
+            anim.SetLayerWeight(anim.GetLayerIndex("Left"), 0);
+            anim.SetLayerWeight(anim.GetLayerIndex("Right"), 1);
+        }
+        else if (deltaAngle >= -120 && deltaAngle <= -60)
+        {
+            anim.SetLayerWeight(anim.GetLayerIndex("Left"), 1);
+            anim.SetLayerWeight(anim.GetLayerIndex("Right"), 0);
+        }
+        else
+        {
+            anim.SetLayerWeight(anim.GetLayerIndex("Left"), 0);
+            anim.SetLayerWeight(anim.GetLayerIndex("Right"), 0);
+        }
+
+        if (moveInput.x == 0 && moveInput.y == 0)
+        {
+            anim.SetInteger("walkingMode", 0);
+            moveVelocity = moveInput.normalized * speed;
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetInteger("walkingMode", 2);
+            moveVelocity = moveInput.normalized * runningSpeed;
+        }
+        else
+        {
+            anim.SetInteger("walkingMode", 1);
+            moveVelocity = moveInput.normalized * speed;
+        }
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+
+        lookDir = mousePos - rb.position;
+        float lookAngle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = lookAngle;
+    }
+}
